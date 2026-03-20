5830d0bfc499c16c6ae63114141a4ce9b792a8a6
MichBurl <michal.burliga03@gmail.com>
2026-03-20 20:39:18 +0100

    quick fix

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  // If you are deploying to username.github.io/repo-name/, 
-  // uncomment the line below and replace 'repo-name' with your actual repository name:
-  // basePath: '/repo-name',
+  basePath: '/jazda-z-bajerem',
+  trailingSlash: true,
 };
 
 export default nextConfig;
