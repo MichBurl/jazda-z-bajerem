3909ebdaa5f57c5a1795f402e1dde48780a8059c
MichBurl <michal.burliga03@gmail.com>
2026-03-21 01:38:26 +0100

    img render problem

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,9 @@ const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  // GitHub Pages requirements
   basePath: isProd ? '/jazda-z-bajerem' : '',
+  assetPrefix: isProd ? '/jazda-z-bajerem/' : '',
   images: {
     unoptimized: true,
   },
